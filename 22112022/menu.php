9744f7b41e825f33b86f604082f20015e701941e
Rafael Silva <rafsilva19@gmail.com>
2022-11-22 20:14:31 +0000

    22112022

---
+++ 22112022/menu.php
@@ -0,0 +1,11 @@
+<ul>
+    <li>
+        <a href="index.php">Home</a>
+    </li>
+    <li>
+        <a href="index2.php">Contactos</a>
+    </li>
+    <li>
+        <a href="index3.php">Localização</a>
+    </li>
+</ul>
